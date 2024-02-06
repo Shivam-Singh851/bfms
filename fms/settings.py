f7811c2340c34b6dd7b8353a77580065f5637b3c
Shivam-Singh851 <37085370+Shivam-Singh851@users.noreply.github.com>
2024-02-06 21:34:24 +0530

    added-require file, db connection

--- fms/settings.py
+++ fms/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'fms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+####################################################### PSQL DB #############################################
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fmsdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Admintechs24',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
